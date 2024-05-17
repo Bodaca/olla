1941d67ee1ab45bdddc2747946a18e1fb51c0b9b
Bodaca <162328848+Bodaca@users.noreply.github.com>
2024-05-18 00:45:43 +0700

    bodaca
    fafafaeg

---
+++ script.js
@@ -0,0 +1,50 @@
+const menuIcon = document.getElementById("menu-icon");
+const menuList = document.getElementById("menu-list");
+
+menuIcon.addEventListener("click", () => {
+  menuList.classList.toggle("hidden");
+});
+
+
+
+
+
+
+
+const filterContainer = document.querySelector(".gallery-filter"),
+galleryItems = document.querySelectorAll(".gallery-item");
+
+filterContainer.addEventListener("click", (event) =>{
+  if(event.target.classList.contains("filter-item")){
+     // deactivate existing active 'filter-item'
+     filterContainer.querySelector(".active").classList.remove("active");
+     // activate new 'filter-item'
+     event.target.classList.add("active");
+     const filterValue = event.target.getAttribute("data-filter");
+     galleryItems.forEach((item) =>{
+      if(item.classList.contains(filterValue) || filterValue === 'all'){
+        item.classList.remove("hide");
+         item.classList.add("show");
+      }
+      else{
+        item.classList.remove("show");
+        item.classList.add("hide");
+      }
+     });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
